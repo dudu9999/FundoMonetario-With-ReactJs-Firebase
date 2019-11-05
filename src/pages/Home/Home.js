4ff912c4de1ccb5791113274f1ceeb8ba3082761
dudu9999 <ecaetanocorrea@gmail.com>
2019-11-04 23:59:50 -0300

    react-firebase-1

---
+++ src/pages/Home/Home.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './Home.css'
+
+const Home = () => {
+    return ( <div className="row">
+        <div className="card">
+            <div className="card-content">
+            </div>
+            
+            <div className="card-footer">
+                <span style={{fontWeight:'bold', color: '#001AA0'}}Meu Fundo></span>
+            </div>
+        </div>
+    </div> );
+}
+ 
+export default Home;
